dc7f1b644103ece2aea2da14ef6ccf7f9a4042e3
MikeTheFyke <mikefyke@hotmail.com>
2020-03-24 11:31:19 -0400

    Added button css

--- logic.js
+++ logic.js
@@ -1,4 +1,8 @@
 var beep = new Audio();
 
-document.getElementById("cowImg").src = "./images/Button.gif";
+
 beep.src = "./audio/Cow_Moo.mp3";
+
+function buttonAnimate () {
+    document.getElementById("cowImg").src = "./images/Button.gif";
+}
