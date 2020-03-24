ab74dc60af0d26ad0b5a323e041675e9ef79d819
MikeTheFyke <mikefyke@hotmail.com>
2020-03-24 11:45:22 -0400

    Added nav container

--- logic.js
+++ logic.js
@@ -1,7 +1,7 @@
-var beep = new Audio();
+var cowSound = new Audio();
 
 
-beep.src = "./audio/Cow_Moo.mp3";
+cowSound.src = "./audio/Cow_Moo.mp3";
 
 function buttonAnimate () {
     document.getElementById("cowImg").src = "./images/Button.gif";
