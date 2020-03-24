80fb29695695d42cbede80dbb33f8a8ff2914731
MikeTheFyke <mikefyke@hotmail.com>
2020-03-24 11:52:43 -0400

    Adde more cowAnimate logic

--- logic.js
+++ logic.js
@@ -3,6 +3,10 @@ var cowSound = new Audio();
 
 cowSound.src = "./audio/Cow_Moo.mp3";
 
+function cowAnimate () {
+    document.getElementById("cowImg").src = "./images/Button.gif";
+}
+
 function buttonAnimate () {
     document.getElementById("cowImg").src = "./images/Button.gif";
 }
