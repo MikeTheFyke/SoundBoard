87ba6b5bd38ba5250f0b3432b9ef4662320cd8fd
MikeTheFyke <mikefyke@hotmail.com>
2020-03-25 16:38:55 -0400

    Added pig snort logic

--- logic.js
+++ logic.js
@@ -1,8 +1,10 @@
 var cowSound = new Audio();
 var chickenSound = new Audio();
+var pigSound = new Audio();
 
 cowSound.src = "./audio/Cow_Moo.mp3";
 chickenSound.src = "./audio/Chicken_Sound.mp3";
+pigSound.src = "./audio/Pig_Snort.mp3";
 
 function cowAnimate () {
     document.getElementById("cowImg").src = "./images/RedButton.gif";
