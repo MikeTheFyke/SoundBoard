5be232de06b0fa90eac148e3efbdc32b5412c9c1
MikeTheFyke <mikefyke@hotmail.com>
2020-03-25 16:42:55 -0400

    Added giraffe sound variables to logic

--- logic.js
+++ logic.js
@@ -2,11 +2,13 @@ var cowSound = new Audio();
 var chickenSound = new Audio();
 var pigSound = new Audio();
 var sheepSound = new Audio();
+var giraffeSound = new Audio();
 
 cowSound.src = "./audio/Cow_Moo.mp3";
 chickenSound.src = "./audio/Chicken_Sound.mp3";
 pigSound.src = "./audio/Pig_Snort.mp3";
 sheepSound.src = "./audio/Sheep_Sound.mp3";
+giraffeSound.src = "./audio/Giraffe_Dying.mp3";
 
 function cowAnimate () {
     document.getElementById("cowImg").src = "./images/RedButton.gif";
