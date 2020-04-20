2f51bcf916fd80cf0da69bdb40ed3097490f477a
MikeTheFyke <mikefyke@hotmail.com>
2020-04-20 15:44:03 -0400

    Added canvas and video elements to html

--- canvasLogic.js
+++ canvasLogic.js
@@ -2,4 +2,6 @@ const video = document.querySelector(".player");
 const canvas = document.querySelector(".photo");
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector(".strip");
-const snap = document.querySelector(".snap");
+// const snap = document.querySelector(".snap");
+
+
